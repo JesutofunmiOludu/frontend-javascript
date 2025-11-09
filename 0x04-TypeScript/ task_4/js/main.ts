d0f9e143b0e811db3f6b433832a861322da92c5d
JesutofunmiOludu <130249061+JesutofunmiOludu@users.noreply.github.com>
2025-11-09 17:13:07 +0100

    Update main.ts

--- 0x04-TypeScript/ task_4/js/main.ts
+++ 0x04-TypeScript/ task_4/js/main.ts
@@ -1,1 +1,11 @@
+// Create and export constants for each subject
+export const cpp = new Subjects.Cpp();
+export const java = new Subjects.Java();
+export const react = new Subjects.React();
 
+// Create and export teacher object with C experience
+export const cTeacher: Subjects.Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  experienceTeachingC: 10
+};
