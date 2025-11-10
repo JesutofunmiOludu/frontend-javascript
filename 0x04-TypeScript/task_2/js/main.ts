a101a908753aad00ee169a5a62fc3dba717b011f
JesutofunmiOludu <130249061+JesutofunmiOludu@users.noreply.github.com>
2025-11-10 10:16:39 +0100

    Create main.ts

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,13 @@
+function isDirector(employee: Director | Teacher): employee is Director{
+    return employee instanceof Director;
+}
+
+function executeWork(employee: Director | Teacher){
+    if (isDirector(employee)) {
+        return employee.workDirectorTasks();
+    }
+    else{
+        return employee.workTeacherTasks();
+    }
+
+}
